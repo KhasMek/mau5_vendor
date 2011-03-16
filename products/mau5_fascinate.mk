192810f848d57c289aa7c6043c0b65a32ea6e966
OMJ <mijames1@gmail.com>
2011-03-16 18:10:20 -0400

    remove a bunch of images that arent needed and mess up build

--- products/mau5_fascinate.mk
+++ products/mau5_fascinate.mk
@@ -3,5 +3,5 @@ $(call inherit-product, device/samsung/fascinate/full_fascinate.mk)
 
 PRODUCT_NAME := mau5_fascinate
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/Mau5/overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/mau5_vendor/overlay
 
