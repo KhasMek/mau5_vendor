1978aa852455c9ebc4991ed71d28059f41235c5d
KhasMek <Boushh@gmail.com>
2011-03-20 14:41:27 -0700

    defining things helps

--- products/mau5.mk
+++ products/mau5.mk
@@ -8,7 +8,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=SometimesThingsGet.ogg
 
 PRODUCT_COPY_FILES += \
-    vendor/mau5/prebuilt/common/sounds/SometimesThingsGet.ogg:system/media/audio/ringtones/SometimesThingsGet.ogg
+    vendor/mau5/prebuilt/common/sounds/SometimesThingsGet.ogg:system/media/audio/ringtones/SometimesThingsGet.ogg \
+    vendor/mau5_OMJ/bootanimation.zip:system/media/bootanimation.zip
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/mau5_OMJ/overlay
 
