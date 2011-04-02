9df99bc7f8b2271a1945040ee30f8c185f2d9f8f
KhasMek <Boushh@gmail.com>
2011-04-02 14:33:57 -0700

    copy, then define, aka - le duh

--- products/mau5.mk
+++ products/mau5.mk
@@ -4,12 +4,12 @@ $(call inherit-product, device/samsung/fascinate/full_fascinate.mk)
 PRODUCT_NAME := mau5_fascinate
 
 # Set Default Ringtone
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=SometimesThingsGet.ogg
-
 PRODUCT_COPY_FILES += \
     vendor/mau5/prebuilt/common/sounds/SometimesThingsGet.ogg:system/media/audio/ringtones/SometimesThingsGet.ogg \
     vendor/mau5_OMJ/bootanimation.zip:system/media/bootanimation.zip
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=SometimesThingsGet.ogg
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/mau5_OMJ/overlay
 
