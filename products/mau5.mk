6e068825bfe4ea803b00c79defd561fcdffbb778
KhasMek <Boushh@gmail.com>
2011-04-04 20:17:17 -0700

    typo

--- products/mau5.mk
+++ products/mau5.mk
@@ -5,7 +5,7 @@ PRODUCT_NAME := mau5_fascinate
 
 # Set Default Ringtone
 PRODUCT_COPY_FILES += \
-    vendor/mau5/prebuilt/common/sounds/SometimesThingsGet.ogg:system/media/audio/ringtones/SometimesThingsGet.ogg \
+    vendor/mau5_OMJ/prebuilt/common/sounds/SometimesThingsGet.ogg:system/media/audio/ringtones/SometimesThingsGet.ogg \
     vendor/mau5_OMJ/bootanimation.zip:system/media/bootanimation.zip
 
 PRODUCT_PROPERTY_OVERRIDES += \
